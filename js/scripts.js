665586ba6c51c887c9ccf73634898331b95debf2
Katrina Hockman and Robert Bruce <robert@>
2018-10-22 11:03:09 -0700

    add html content and array js functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,25 @@
 $(function() {
+      $("#append-here").hide();
+
+      var blanks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+      $("#grocery-list").submit(function(event) {
+
+          event.preventDefault();
+          var groceryList = blanks.map(function(blank) {
+            return $("#input" + blank).val();
+          });
+          $("#grocery-input").hide();
+          $("#append-here").show();
+
+          groceryList.sort();
+          groceryList.forEach(function(each) {
+            if (each !== ""){
+
+              $("#append-here").append("<li>" + each.toUpperCase() + "</li>")
+            }
+            });
+          });
 
 
-
-});
+      });
